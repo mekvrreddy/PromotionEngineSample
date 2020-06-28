0caebf6bfc1024c5e87b7a374c9933ee7e8e09bf
Venkata Ramana Reddy K <ramanareddy.k@gmail.com>
2020-06-28 23:56:59 +0530

    Added Promotion class and added required attributes to include both fixed price and also percentage promotions and also maintianed activation date and also obsolete dates to identify the active promotions

---
+++ PromotionEngineSample/PromotionEngineSample/Promotion.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PromotionEngineSample
+{
+    public class Promotion
+    {
+        public int Id { get; set; }
+
+        public Dictionary<Product, int> Products { get; set; }
+
+        public decimal PromoPrice { get; set; }
+
+        public DateTime ActivationDate { get; set; }
+
+        public DateTime? ObsoleteDate { get; set; }
+
+        public bool IsFixedPrice { get; set; }
+
+        public Promotion(int id, Dictionary<Product, int> products, decimal promoPrice, DateTime activationDate, bool isfixedprice, DateTime? obsoleteDate)
+        {
+            this.Id = id;
+            this.Products = products;
+            this.PromoPrice = promoPrice;
+            this.ActivationDate = activationDate;
+            this.IsFixedPrice = isfixedprice;
+            if (obsoleteDate.HasValue)
+            {
+                this.ObsoleteDate = obsoleteDate;
+            }
+        }
+
+    }
+}
