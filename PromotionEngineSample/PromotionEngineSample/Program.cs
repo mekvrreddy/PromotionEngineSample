709b722003dd21260c5a366d673defb438fa503a
Venkata Ramana Reddy K <ramanareddy.k@gmail.com>
2020-06-29 00:52:02 +0530

    Verified 3rd scenario where 55 rebate applied so it returned 280

--- PromotionEngineSample/PromotionEngineSample/Program.cs
+++ PromotionEngineSample/PromotionEngineSample/Program.cs
@@ -20,6 +20,15 @@ namespace PromotionEngineSample
                                                               });
             decimal finalPrice2 = GetFinalPrice(order5A5B1C);
             #endregion
+
+            #region "Scenario 3A, 5B, 1C and 1D"
+            Order order3A5B1C1D = new Order(1, new List<Product>() { new Product("A"), new Product("A"), new Product("A"),
+                                                              new Product("B"), new Product("B"), new Product("B"),
+                                                              new Product("B"), new Product("B"), new Product("C"),
+                                                              new Product("D")
+                                                              });
+            decimal finalPrice3 = GetFinalPrice(order3A5B1C1D);
+            #endregion
         }
 
         public static decimal GetFinalPrice(Order order)
