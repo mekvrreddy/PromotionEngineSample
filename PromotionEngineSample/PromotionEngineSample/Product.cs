87d0fb243bd7651363ba165e34e7dd6d506f0897
Venkata Ramana Reddy K <ramanareddy.k@gmail.com>
2020-06-28 23:50:36 +0530

    Added Prodjut class to define Unit price for various SKU ids

---
+++ PromotionEngineSample/PromotionEngineSample/Product.cs
@@ -0,0 +1,37 @@
+﻿namespace PromotionEngineSample
+{
+    public class Product
+    {
+        public string SKUId { get; set; }
+
+        public decimal UnitPrice { get; set; }
+
+        public Product(string skuId)
+        {
+            this.SKUId = skuId;
+
+            switch (skuId)
+            {
+                case "A":
+                case "a":
+                    this.UnitPrice = 50M;
+                    break;
+
+                case "B":
+                case "b":
+                    this.UnitPrice = 30M;
+                    break;
+
+                case "C":
+                case "c":
+                    this.UnitPrice = 20M;
+                    break;
+
+                case "D":
+                case "d":
+                    this.UnitPrice = 15M;
+                    break;
+            }
+        }
+    }
+}
