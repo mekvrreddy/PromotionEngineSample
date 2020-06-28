103eca3d96ee92b753b45e08f8fc02087a52d4d9
Venkata Ramana Reddy K <ramanareddy.k@gmail.com>
2020-06-29 00:22:45 +0530

    Tested Scenario A where no promotion aplied

--- PromotionEngineSample/PromotionEngineSample/Program.cs
+++ PromotionEngineSample/PromotionEngineSample/Program.cs
@@ -1,9 +1,27 @@
-﻿namespace PromotionEngineSample
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace PromotionEngineSample
 {
     class Program
     {
         static void Main(string[] args)
         {
+            #region "Scenario A"
+            Order orderA = new Order(1, new List<Product>() { new Product("A"), new Product("B"), new Product("C") });
+            decimal finalPrice = GetFinalPrice(orderA);
+            #endregion
         }
+
+        public static decimal GetFinalPrice(Order order)
+        {
+            List<decimal> promoprices = PromotionManager.GetActivePromotions()
+                    .Select(promo => PromotionManager.GetTotalPrice(order, promo))
+                    .ToList();
+            decimal origprice = order.Products.Sum(x => x.UnitPrice);
+            decimal promoprice = promoprices.Sum();
+            return origprice - promoprice;
+        }
+
     }
 }
