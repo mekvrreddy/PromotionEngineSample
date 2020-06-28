b9f7eca3156033ac84ec51ecc28831347bfebb72
Venkata Ramana Reddy K <ramanareddy.k@gmail.com>
2020-06-29 00:00:49 +0530

    Added Order class and required attributes to maintain the order specific Products

---
+++ PromotionEngineSample/PromotionEngineSample/Order.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace PromotionEngineSample
+{
+    public class Order
+    {
+        public int Id { get; set; }
+
+        public List<Product> Products { get; set; }
+
+        public Order(int id, List<Product> products)
+        {
+            this.Id = id;
+            this.Products = products;
+        }
+    }
+}
